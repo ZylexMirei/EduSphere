af4d3aa4369458f3916645f5968d6c66825888bc
ZylexMirei <salcedomonse678@gmail.com>
2025-11-26 15:20:09 -0400

    Cambios en mi programa principal para app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path'; // <-- 1. ¡IMPORTA 'PATH'!
-import { fileURLToPath } from 'url'; // <-- 1. ¡IMPORTA ESTO!
+import path from 'path'; 
+import { fileURLToPath } from 'url';
 
-// Importa tus rutas (esto se queda igual)
+
 import authRoutes from './routes/auth.routes.js';
 import materialRoutes from './routes/material.routes.js';
 import examRoutes from './routes/exam.routes.js';
@@ -12,10 +12,9 @@ import assistantRoutes from './routes/assistant.routes.js';
 import dashboardRoutes from './routes/dashboard.routes.js';
 import adminRoutes from './routes/admin.routes.js';
 
-// --- Configuración para __dirname ---
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// ------------------------------------
 
 const app = express();
 
@@ -25,9 +24,6 @@ app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 
-// --- ¡CARPETA PÚBLICA "WAOOO"! ---
-// 2. Sirve los archivos de la carpeta 'uploads' como si estuvieran en la raíz
-// (Esto es lo que permite que `http://localhost:3001/uploads/mi-pdf.pdf` funcione)
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 // --------------------------------
 
